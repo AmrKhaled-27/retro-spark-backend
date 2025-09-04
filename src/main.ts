12bea4cf3c219566e729bceaba5ba41c81467ffc
Ahmed Khaled <ahmedkhaled.m24@gmail.com>
2025-09-04 15:49:40 +0300

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: true, credentials: true });
 
   // Global pipes
   app.useGlobalPipes(
