3890a93afdd7880bdb318fabf5ee81f98f603e18
Ahmed Khaled <ahmedkhaled.m24@gmail.com>
2025-10-11 14:13:27 +0300

    feat: update current user decorator
    update current user decorator to work with socket in addition to http

--- src/common/decorators/current-user.decorator.ts
+++ src/common/decorators/current-user.decorator.ts
@@ -1,9 +1,21 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { AuthenticatedUser } from '../../types/auth.types';
+import { Request } from 'express';
+import { SocketWithUser } from 'src/types/socket';
 
 export const CurrentUser = createParamDecorator(
   (data: unknown, ctx: ExecutionContext): AuthenticatedUser => {
-    const request = ctx.switchToHttp().getRequest();
-    return request.user;
+    const request = ctx.switchToHttp().getRequest<Request>(); // HTTP
+    const socket = ctx.switchToWs().getClient<SocketWithUser>(); // Socket
+
+    if (request) {
+      return request.user;
+    }
+
+    if (socket) {
+      return socket.user;
+    }
+
+    return null;
   },
 );
